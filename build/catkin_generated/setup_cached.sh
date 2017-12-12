b997316ba28cf697fbfe194407502eb83cf16cda
Jaerock Kwon <jkwon@kettering.edu>
2017-12-12 03:51:45 -0500

    Milestone 1: It works!

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,9 +8,10 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/jaerock/mirvehicle_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib:/home/jaerock/mirvehicle_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib:/usr/local/cuda-8.0/lib64"
-export PATH="/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/kinetic/bin:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/usr/local/cuda-8.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/jaerock/anaconda3/bin"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib:/home/jaerock/mirvehicle_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib"
+export PATH="/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/kinetic/bin:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/home/jaerock/software/blender:/usr/local/cuda-9.0/bin:/home/jaerock/bin:/home/jaerock/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
 export PKG_CONFIG_PATH="/home/jaerock/mirvehicle_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/jaerock/mirvehicle_ws/build"
 export PYTHONPATH="/home/jaerock/mirvehicle_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/jaerock/mirvehicle_ws/devel/share/common-lisp"
 export ROS_PACKAGE_PATH="/home/jaerock/mirvehicle_ws/src:$ROS_PACKAGE_PATH"
